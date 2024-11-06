5210864721c70f402f1a473df65c56defb404d37
Sujith JosephIrudaya Antonyraj <sujith.joseph.antonyraj@rabobank.nl>
2024-11-06 17:43:04 +0530

    adding authtoken

--- finance_portfolio/src/app/app.config.ts
+++ finance_portfolio/src/app/app.config.ts
@@ -1,11 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import {  Router, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { interceptorInterceptor } from './interceptors/interceptor.interceptor';
+import { Observable, catchError, map } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient(withInterceptorsFromDi(),withInterceptors([interceptorInterceptor]),)]
+  providers: [provideRouter(routes), {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      multi: true,
+      deps: [HttpClient],
+    }, provideAnimationsAsync(),provideHttpClient(withInterceptorsFromDi(),withInterceptors([interceptorInterceptor]),)]
 };
+function initializeAppFactory(httpClient: HttpClient,route:Router): () => Observable<any> {
+  return () => httpClient.get("http://localhost:3000/authToken")
+    .pipe(
+      map((data:any)=>{
+        localStorage.setItem("authToken",data.token)
+        return true;
+      }),
+      catchError(error => {
+
+        route.navigate(['/error']); 
+
+        throw error; 
+
+      })
+    )
+ }
